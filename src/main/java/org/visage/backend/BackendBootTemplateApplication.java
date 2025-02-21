de3e9e05a39365d5af404959e61ee7c661c09617
VisaZhou <472493922@qq.com>
2025-02-21 14:25:57 +0800

    首次提交

---
+++ src/main/java/org/visage/backend/BackendBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.visage.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBootTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/visage/backend/BackendBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.visage.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
