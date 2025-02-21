edd0e9d303ee4424b2bd6e40248f08d8bdec68d3
VisaZhou <472493922@qq.com>
2025-02-21 15:36:08 +0800

    【swagger】加入swagger配置，开放获取版本号接口

---
+++ src/main/java/org/visage/backend/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package org.visage.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/getVersion")
+    public String hello() {
+        return "version：0.0.1";
+    }
+}
