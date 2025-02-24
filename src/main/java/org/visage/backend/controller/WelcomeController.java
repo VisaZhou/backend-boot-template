7d076a2a0bc69a9b400fbae0cf0057cf4fc3341d
VisaZhou <472493922@qq.com>
2025-02-24 22:55:53 +0800

    【Kubernetes】控制台打印，测试集群副本负载均衡

--- src/main/java/org/visage/backend/controller/WelcomeController.java
+++ src/main/java/org/visage/backend/controller/WelcomeController.java
@@ -1,13 +1,21 @@
 package org.visage.backend.controller;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class WelcomeController {
 
     @GetMapping("/getVersion")
     public String hello() {
-        return "version：0.0.1";
+        // 加载项目根目录下的 .env 文件
+        Dotenv dotenv = Dotenv.load();
+        // 读取 .env 文件中的变量
+        String projectVersion = dotenv.get("PROJECT_VERSION");
+        // 控制台打印，测试集群副本
+        System.out.println("version: " + projectVersion);
+        return "version：".concat(projectVersion);
     }
 }
