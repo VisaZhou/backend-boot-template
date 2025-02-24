dfce30dde313c8bede73caab27a31925eed3697f
VisaZhou <472493922@qq.com>
2025-02-25 00:07:14 +0800

    【Kubernetes】版本升级，去除dotenv

--- src/main/java/org/visage/backend/controller/WelcomeController.java
+++ src/main/java/org/visage/backend/controller/WelcomeController.java
@@ -1,6 +1,5 @@
 package org.visage.backend.controller;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +9,6 @@ public class WelcomeController {
 
     @GetMapping("/getVersion")
     public String hello() {
-        // 加载项目根目录下的 .env 文件
-        Dotenv dotenv = Dotenv.load();
-        // 读取 .env 文件中的变量
-        String projectVersion = dotenv.get("PROJECT_VERSION");
-        // 控制台打印，测试集群副本
-        System.out.println("version: " + projectVersion);
-        return "version：".concat(projectVersion);
+        return "version：0.0.2";
     }
 }
