de80110a8e04af623887e73d7c5b8520856fcf75
VisaZhou <472493922@qq.com>
2025-02-25 08:58:51 +0800

    【Kubernetes】测试：接口返回pod名称

--- src/main/java/org/visage/backend/controller/WelcomeController.java
+++ src/main/java/org/visage/backend/controller/WelcomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/getVersion")
-    public String hello() {
+    @GetMapping("/getPodName")
+    public String getPodName() {
         // 获取当前 Pod 名称
         String podName = System.getenv("HOSTNAME");
         return "Handled by Pod: " + podName;
